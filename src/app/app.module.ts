5e1f069df08d522b302f435cdbd69b52d89130b0
Jaime Torres <jatorres@theworkshop.com>
2019-09-23 16:41:00 +0200

    First login addeed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { LayoutComponent } from "./layout/layout.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [LayoutComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatMenuModule],
   providers: [],
   bootstrap: [LayoutComponent]
 })
