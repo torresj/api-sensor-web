6b22f9c9e7b964ef3246b8df2b23cd7c539a067f
Jaime Torres <jatorres@theworkshop.com>
2019-09-24 15:41:32 +0200

    login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatMenuModule } from "@angular/material/menu";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { LayoutComponent } from "./layout/layout.component";
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from "./login/login.component";
+
+// Material
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
+
+// Flex layout
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [LayoutComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatMenuModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [LayoutComponent]
 })
