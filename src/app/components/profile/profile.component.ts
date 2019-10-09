98ad2688fd6d53138e14ef05781b6d2df91f5f6b
Jaime Torres <jatorres@theworkshop.com>
2019-10-09 10:24:03 +0200

    Responsive subtitle page added

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AppStore } from "src/app/models/stores/appstore";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public store: AppStore) {
+    store.page = "Perfil";
   }
 
+  ngOnInit() {}
 }
